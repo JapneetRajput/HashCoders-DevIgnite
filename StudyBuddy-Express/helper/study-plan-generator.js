4df2a1bdbf9645a1ed837a9a2b448bdb7a10cd45
JapneetRajput <japneetrajput2804@gmail.com>
2024-03-02 17:16:04 +0530

    re init

---
+++ StudyBuddy-Express/helper/study-plan-generator.js
@@ -0,0 +1,5 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "MODEL_NAME" });

f6cfc79962c31eff76cc22375cdef5eb64d4f268
yashviiii <yashvi.dhar@gmail.com>
2024-03-03 05:22:39 +0530

    refactoring

--- StudyBuddy-Express/app.js
+++ StudyBuddy-Express/app.js
@@ -11,6 +11,7 @@ import bodyParser from "body-parser";
 // const upload = multer({ dest: "uploads/" });
 // const Axios = require("axios");
 import connectDB from "./config/database.js";
+import { searchYT,displayYTResults,aiRank } from "./helper/yt.js";
 
 // Connect mongoose server
 connectDB();
@@ -30,6 +31,31 @@ import usersRoutes from "./routes/users.js";
 
 // Mount the users routes at the /users path
 app.use("/api/users", usersRoutes);
+
+app.post("/get-video-resources",async(req,res)=>{
+  const query = req.body.query
+  const duration = req.body.duration
+  const depth = req.body.depth
+  try {
+    // Search YouTube for videos related to "big data analytics"
+    const response = await searchYT(query,5,duration,depth);
+
+    console.log(response);
+    console.log("128");
+    // Display YouTube search results and extract transcripts
+    const result = await displayYTResults(response);
+    console.log(result)
+
+    // Rank the transcripts based on duration and depth
+    const [id1,id2] = await aiRank();
+    console.log(id1," ",id2)
+
+    // console.log("Res:", res);
+    return res.send("Success")
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})
 
 // Start the server
 const PORT = process.env.PORT || 3001;
